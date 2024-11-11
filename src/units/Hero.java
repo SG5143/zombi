3ff76595fb54de1d9de7cf28248b238bd2b32c71
SG5143 <sun5143a@gmail.com>
2024-11-11 17:07:38 +0900

    Add potion member field, getter, and recovery method to Hero class

--- src/units/Hero.java
+++ src/units/Hero.java
@@ -3,9 +3,22 @@ package units;
 import interfaces.Enemy;
 
 public class Hero extends Unit {
+	int potion;
 
 	public Hero(int position, int hp, int attackPower) {
 		super(position, hp, attackPower);
+		potion = 2;
+	}
+
+	public int getPotionCnt() {
+		return potion;
+	}
+
+	public void recovery() {
+		if (potion > 0) {
+			potion--;
+			increaseHp(50);
+		}
 	}
 
 	public void move() {
@@ -18,16 +31,16 @@ public class Hero extends Unit {
 		if (unit instanceof Enemy) {
 			int damage = r.nextInt(attackPower / 2) + (attackPower / 2) + 1;
 
-			if(unit instanceof Boss && ((Boss) unit).getShield() > 0) {
+			if (unit instanceof Boss && ((Boss) unit).getShield() > 0) {
 				Boss boss = (Boss) unit;
 				boss.decreaseShield(damage);
-			}else {
+			} else {
 				if (r.nextBoolean())
 					unit.decreaseHp(damage * 2);
 				else
-					unit.decreaseHp(damage);				
+					unit.decreaseHp(damage);
 			}
-			
+
 			System.out.println("적에게 " + damage + "의 피해를 가했습니다");
 		}
 	}
