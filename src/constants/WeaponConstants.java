e8d49f0463d626e828f8e14eeae46932d970146c
SG5143 <sun5143a@gmail.com>
2024-11-12 10:52:06 +0900

    Add class to store weapon constants

---
+++ src/constants/WeaponConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class WeaponConstants {
+	public static final String[][] WEAPONS = { { "우드 검", "5" }, { "돌 검", "10" }, { "철 검", "20" }, { "금 검", "40" },
+			{ "다이아 검", "80" } };
+}
